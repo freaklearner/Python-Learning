804b8c9f5ed5e7e0d0cb673b8a3dc85906ca3e2f
freaklearner <shub.shubhamthakur@gmail.com>
2018-03-22 02:38:43 +0530

    list been practised

---
+++ list.py
@@ -0,0 +1,18 @@
+mylist = []
+
+mylist.append("Hello")
+mylist.append("World")
+mylist.append("How")
+mylist.append("you")
+mylist.append("doing")
+
+str = ""
+for x in mylist:
+	str = str + x + " "
+
+print(str)
+
+fruits = ['Apple','Mango','Grapes']
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
