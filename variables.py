607e100399eda9ea87b8627a1c1630c2520b7908
freaklearner <shub.shubhamthakur@gmail.com>
2018-03-22 02:29:53 +0530

    practise variables and tried other version of print

---
+++ variables.py
@@ -0,0 +1,16 @@
+mystring = None
+myfloat = None
+myint = None
+
+mystring = "Hello World"
+print("String: %s " % mystring)
+
+myfloat = float(10)
+print("Float: %.2f" % myfloat)
+
+myint = 15
+print("Int: %d" % myint)
+
+
+a,b = 3,4
+print(a,b)
