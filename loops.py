5b2cf03c268477847fc783660a111abe7d6b7d7f
freaklearner <shub.shubhamthakur@gmail.com>
2018-03-24 00:46:58 +0530

    Loops

---
+++ loops.py
@@ -0,0 +1,25 @@
+primes=[2,3,5,7,11]
+
+for x in primes:
+    print(x)
+
+    
+for x in range(5):
+    print(x)
+
+for x in range(3,5):
+    print(x)
+
+
+for x in range(0,8,2):
+    print(x)
+
+
+count = 0;
+while count<5:
+    print("count: %d" % count)
+    count +=2
+else:
+    print("count reached %d" % count)
+    
+    
