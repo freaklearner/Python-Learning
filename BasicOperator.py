cc730ebb30b44ce2e0f1617ecaa2c84db3b1b01c
freaklearner <shub.shubhamthakur@gmail.com>
2018-03-23 14:55:15 +0530

    practise Basic operators and string formating

---
+++ BasicOperator.py
@@ -0,0 +1,24 @@
+remainder = 11%3
+print(remainder)
+
+square7 = 7 ** 2
+print("Square of 7 %d" % square7)
+
+cube4 = 4 ** 3
+print("Cube of 4 %d" % cube4)
+
+
+
+strRepeate = "hello" * 10
+print(strRepeate)
+
+
+evenNum = [2,4,6,8,10]
+oddNum = [1,3,5,7,9]
+combine = evenNum + oddNum
+print(combine)
+
+
+print([1,2,3] * 3)
+
+
---
+++ StringFormation.py
@@ -0,0 +1,8 @@
+name = "xyz"
+age = 21
+print("%s is %d years old" % (name,age)
+
+
+print("length of 'hello World!' %d " % len('hello World!'))
+
+
