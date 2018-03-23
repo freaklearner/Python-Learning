c83f5a9c5318c14c788e902871e098b16276fdc8
freaklearner <shub.shubhamthakur@gmail.com>
2018-03-23 23:55:07 +0530

    Some operations of string

--- StringFormation.py
+++ StringFormation.py
@@ -1,8 +1,35 @@
 name = "xyz"
 age = 21
-print("%s is %d years old" % (name,age)
+print("%s is %d years old" % (name,age))
+
+astring = "hello world"
+print("length of 'hello World!' %d " % len("hello World!"))
 
 
-print("length of 'hello World!' %d " % len('hello World!'))
+print("Index of 'o' %d" % astring.index('o'))
+
+
+print("Count 'l' %d" % astring.count('l'))
+
+
+print(astring[3:7])
+
+
+print(astring[3:7:2])
+
+
+print(astring[::-1])
+
+
+print(astring.upper())
+
+
+print(astring.lower())
+
+splitstring = astring.split(" ")
+
+for x in splitstring:
+	print(x)
+
 
 
