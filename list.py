f914e9b018be0a4f103a89666f964fefdeb6e057
freaklearner <shub.shubhamthakur@gmail.com>
2018-03-31 03:13:48 +0530

    check list contains particular element

--- list.py
+++ list.py
@@ -1,3 +1,7 @@
+import sys
+sys.path.append(r'/usr/lib/python2.7/dist-packages/')
+import numpy as nm
+
 mylist = []
 
 mylist.append("Hello")
@@ -5,7 +9,7 @@ mylist.append("World")
 mylist.append("How")
 mylist.append("you")
 mylist.append("doing")
-
+mylist.sort
 str = ""
 for x in mylist:
 	str = str + x + " "
@@ -13,6 +17,25 @@ for x in mylist:
 print(str)
 
 fruits = ['Apple','Mango','Grapes']
+
 print(fruits[0])
 print(fruits[1])
 print(fruits[2])
+
+
+#check if list contains an item
+if 'Orange' in fruits:
+        print("It contains Apples")
+else:
+        print("It doesn't contains Apples")
+
+#sum of the elements of the list
+
+d = []
+
+
+for x in range(2,6,2):
+        d.append(x)
+
+ar = nm.array(d)
+print(ar.sum())
