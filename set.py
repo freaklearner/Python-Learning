5928765c9a4f4dadec87dc34c0c4cec8bc69b61f
shubham <shub.shubhamthakur@gmail.com>
2018-04-08 19:52:17 +0530

    practising set and its methods

---
+++ set.py
@@ -0,0 +1,8 @@
+num = {1,2,3,4,5,5,5,5,5}
+print(num)
+
+# remove duplicates from the string using set but the order get changed
+name = 'shubhssssshhhnnhhuubb'
+rm_dup = set(name)
+name = ''.join(rm_dup)
+print(name)
