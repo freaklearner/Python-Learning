2762c68505ee25ea3f72f9510a75431f019106d1
shubham <shub.shubhamthakur@gmail.com>
2018-04-14 09:36:17 +0530

    changes made to extract content program

--- mapreduce/extractContent.py
+++ mapreduce/extractContent.py
@@ -3,17 +3,22 @@ from os import path
 import extractwordsfile as ewf
 import reducer as rd
 import mapper as m
+from csv import writer
+import replaceCharacter as rc
 files_path =  path.join(os.getcwd(),'filescollection')  #str(os.getcwd()) + '/filescollection'
-print(files_path)
+#print(files_path)
 files = [f for f in os.listdir(files_path) if path.isfile(path.join(files_path,f))]
 content = []
 for fl in files:
-    content.extend(ewf.extractwordsfromfile(path.join(files_path,fl)))
+    pt = path.join(files_path,fl)
+    #rc.replaceChar(pt,"'",'"') 
+    content.extend(ewf.extractwordsfromfile(pt))
 dic = rd.reducer(content)
-fl = open(path.join(os.getcwd(),'wordscount.txt'),'w')
-for key in dic.keys():
-    if len(key) > 0:
-        data = str(key)+'='+str(dic.get(key))+'\n'
-        fl.write(data)
-fl.close()
+fl = path.join(os.getcwd(),'filescollection/wordscount.csv')
+with open(fl,'w') as file:
+    csv_write = writer(file)
+    csv_write.writerow(['WORDS','COUNT'])
+    for key,value in dic.items():
+        if len(key) > 0:
+            csv_write.writerow([key,value])
 print('FILE CREATED SUCCESSFULLY')
