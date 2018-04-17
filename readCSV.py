2bd8ad05f8a96ce953910cc30ff01c84aadaf90d
shubham <shub.shubhamthakur@gmail.com>
2018-04-17 22:41:01 +0530

    practise modification

--- readCSV.py
+++ readCSV.py
@@ -3,6 +3,5 @@ from os import path
 import os
 with open(path.join(os.getcwd(),'files/repoData.txt')) as file:
     csv_reader = reader(file, delimiter=',' )
-    csv_reader = list(file)
     for row in csv_reader:
         print(row)
