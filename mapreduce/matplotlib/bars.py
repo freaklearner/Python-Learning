6f578e399c75208dcdf2e12203e3e2347d7da1f0
shubham <shub.shubhamthakur@gmail.com>
2018-04-19 02:29:43 +0530

    plot bar

---
+++ mapreduce/matplotlib/bars.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as ppt
+
+x = [1,5,8]
+y = [10,20,5]
+
+x2 = [1,5,8]
+y2 = [10,18,7]
+ppt.bar(x,y,label='Trump',color='r')
+ppt.bar(x2,y2,label='Modi',color='pink' )
+ppt.legend()
+ppt.title('tweets')
+ppt.show()
